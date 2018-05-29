9d30b4f589c24928875556463d818750aeb2ffe5
Esio Freitas <esio.gustavo@gmail.com>
2018-05-29 09:57:50 -0300

    Adding Player

--- StellarBattle/src/Controller/Player.java
+++ StellarBattle/src/Controller/Player.java
@@ -3,4 +3,78 @@ package Controller;
 
 public class Player {
     
+    private String name;
+    private int pointers; 
+    private int numberShots;
+    private int numberPowerSee;
+    private int numberPowerDestroy2x2;
+    private int numberPowerDestroyRow;
+    private int numberPowerDestroyCol;
+    
+    public Player(){
+        
+    }
+    
+    //metodos: shot(), powerSee(), powerDestroy - 2x2;Row;Col-()
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPointers() {
+        return pointers;
+    }
+
+    public void setPointers(int pointers) {
+        this.pointers = pointers;
+    }
+
+    public int getNumberShots() {
+        return numberShots;
+    }
+
+    public void setNumberShots(int numberShots) {
+        this.numberShots = numberShots;
+    }
+
+    public int getNumberPowerSee() {
+        return numberPowerSee;
+    }
+
+    public void setNumberPowerSee(int numberPowerSee) {
+        this.numberPowerSee = numberPowerSee;
+    }
+
+    public int getNumberPowerDestroy2x2() {
+        return numberPowerDestroy2x2;
+    }
+
+    public void setNumberPowerDestroy2x2(int numberPowerDestroy2x2) {
+        this.numberPowerDestroy2x2 = numberPowerDestroy2x2;
+    }
+
+    public int getNumberPowerDestroyRow() {
+        return numberPowerDestroyRow;
+    }
+
+    public void setNumberPowerDestroyRow(int numberPowerDestroyRow) {
+        this.numberPowerDestroyRow = numberPowerDestroyRow;
+    }
+
+    public int getNumberPowerDestroyCol() {
+        return numberPowerDestroyCol;
+    }
+
+    public void setNumberPowerDestroyCol(int numberPowerDestroyCol) {
+        this.numberPowerDestroyCol = numberPowerDestroyCol;
+    }
+    
+    
+    
+    
+    
 }
--- StellarBattle/src/Controller/Spacecraft.java
+++ StellarBattle/src/Controller/Spacecraft.java
@@ -2,6 +2,7 @@
 package Controller;
 
 public class Spacecraft {
+    
         public static final int FIRSTSPACECRAFT = 1;
 	public static final int SECONDSPACECRAFT = 2;
 	public static final int THIRDSPACECRAFT = 3;
